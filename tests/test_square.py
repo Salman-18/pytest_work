b5d7bd023e60fff4f90484649092aa9a6d1cd556
Salman-18 <salmannizamani205@gmail.com>
2023-12-21 12:01:35 +0500

    delete one square

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,7 +1,7 @@
 import pytest
 import src.shapes as shapes 
 
-@pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81),(6,36), (7,49), (2, 4), (20, 400), (17, 289), (13, 169), (12, 144), (100, 1000)])
+@pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81),(6,36), (2, 4), (20, 400), (17, 289), (13, 169), (12, 144), (100, 1000)])
 def test_multiple_square_areas(side_length, expected_area):
     assert shapes.Square(side_length).area() == expected_area
 
